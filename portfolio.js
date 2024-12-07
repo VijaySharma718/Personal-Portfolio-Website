8711a52c97c22a631f008a72b68e0b586fcb5532
Vijay Sharma <sharmavijayknp718@gmail.com>
2024-12-07 18:30:50 +0530

    file update

--- portfolio.js
+++ portfolio.js
@@ -1,41 +1,56 @@
-// navbar 
+// navbar
 
-let texts = Array.from(document.querySelectorAll('.container h3'));
+let texts = Array.from(document.querySelectorAll(".container h3"));
 let i = 1; // starting from 1 because 0 already has is-visible, it would cause slight delay otherwise.
 
 setInterval(() => {
-  texts.forEach(text => {
-    text.classList.remove('info-about');
+  texts.forEach((text) => {
+    text.classList.remove("info-about");
   });
-  texts[i].classList.add('info-about');
+  texts[i].classList.add("info-about");
   i += 1;
   if (i >= texts.length) {
     i = 0;
   }
 }, 2000);
 
-
-
-
-
 // text animation
 
-let menuListbutton = document.getElementById("menuListbutton")
-        menuListbutton.style.maxHeight = "0px";
+// let menuListbutton = document.getElementById("menuListbutton")
+//         menuListbutton.style.maxHeight = "0px";
 
-        function toggleMenu(){
-            if(menuListbutton.style.maxHeight == "0px")
-            {
-                menuListbutton.style.maxHeight = "300px";
-            }
-            else{
-                menuListbutton.style.maxHeight = "0px";
-            }
-          }
+//         function toggleMenu(){
+//             if(menuListbutton.style.maxHeight == "0px")
+//             {
+//                 menuListbutton.style.maxHeight = "300px";
+//             }
+//             else{
+//                 menuListbutton.style.maxHeight = "0px";
+//             }
+//           }
+
+// const menuButton = document.getElementById('menubutton');
+// const menuList = document.getElementById('menuListbutton');
+
+// menubutton.addEventListener('click', () => {
+//   menuListbutton.classList.toggle('active');
+// });
 
 
 
-          
 
 
-          
+
+
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
